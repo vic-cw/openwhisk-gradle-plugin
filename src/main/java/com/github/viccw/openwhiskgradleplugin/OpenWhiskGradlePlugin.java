e8db47bc438ff2fb8e3f56c0c96ff4877663d330
vic-cw <vic-cw@users.noreply.github.com>
2019-05-02 18:14:11 +0200

    Add basic structure to publish and apply plugin

---
+++ src/main/java/com/github/viccw/openwhiskgradleplugin/OpenWhiskGradlePlugin.java
@@ -0,0 +1,13 @@
+package com.github.viccw.openwhiskgradleplugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class OpenWhiskGradlePlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+
+}
